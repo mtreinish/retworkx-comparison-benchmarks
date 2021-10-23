56375650993ab7b469b315ba45a124ba2343038f
Matthew Treinish <mtreinish@kortar.org>
2021-10-23 06:52:01 -0400

    Add initial pass at isomorphism benchmark
    This commit adds a new benchmark for comparison, subgraph isomorphism.
    It uses the ARG database of known subgraph isomorphic graphs to time how
    long each library takes to compute 2 graphs are node induced subgraph
    isomorphic. This benchmark hasn't been finalized yet primarily because
    it's slow and I haven't validated the graph-tool variant yet. Once I get
    a feel for how much time this takes and pick a more useable working set
    of input data I'll add results from the benchmark system.

---
+++ retworkx_bench/isomorphism.py
@@ -0,0 +1,75 @@
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from collections import defaultdict
+import csv
+import os
+import statistics
+import sys
+import time
+
+import retworkx
+
+import graphsdb_parser
+
+# 126GB
+MEM_SIZE = 1.26 * 10 ** 11
+
+group_types = {
+    "rand": ["r001", "r005", "r01", "r02"],
+    "m2D": ["m2D", "m2Dr2", "m2Dr4", "m2Dr6"],
+    "m3D": ["m3D", "m3Dr2", "m3Dr4", "m3Dr6"],
+    "m4D": ["m4D", "m4Dr2", "m4Dr4", "m4Dr6"],
+    "bvg": ["b03", "b03m", "b06", "b06m", "b09", "b09m"],
+}
+
+
+def main():
+    path = sys.argv[1]
+    subgraph_prefix = ["si2", "si4", "si6"]
+    subgraph_iso_times = defaultdict(list)
+    with open(f"retworkx_subgraph_iso.csv", "w") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["Graph", "avg_time"])
+        for prefix in subgraph_prefix:
+            for group, g_types in group_types.items():
+                for g_type in g_types:
+                    graph_dir_path = os.path.join(path, prefix, group, g_type)
+                    if not os.path.isdir(graph_dir_path):
+                        continue
+                    for filename in os.listdir(graph_dir_path):
+                        if "B" in filename:
+                            continue
+                        graph_files = [filename.replace("A", "B"), filename]
+                        graph_name = filename
+                        graphs = []
+                        for graph_file in graph_files:
+                            graph_file_path = os.path.join(graph_dir_path, graph_file)
+                            graphs.append(
+                                graphsdb_parser.parse_unlabeled_graphdb_from_file(
+                                    graph_file_path, directed=False
+                                )
+                            )
+                        for i in range(5):
+                            start = time.time()
+                            res = retworkx.graph_is_subgraph_isomorphic(*graphs)
+                            stop = time.time()
+                            if not res:
+                                error_str = " ".join(graph_files)
+                                raise Exception("%s should be isomorphic" % error_str)
+                            subgraph_iso_times[graph_name].append(stop - start)
+                        csv_writer.writerow(
+                            [
+                                graph_name,
+                                statistics.mean(subgraph_iso_times[graph_name]),
+                            ]
+                        )
+
+
+if __name__ == "__main__":
+    main()
--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -15,6 +15,16 @@ wget http://users.diag.uniroma1.it/challenge9/data/USA-road-t/USA-road-t.USA.gr.
 wget http://users.diag.uniroma1.it/challenge9/data/USA-road-t/USA-road-t.NY.gr.gz
 popd
 popd
+mkdir arg_db
+pushd arg_db
+wget https://mivia.unisa.it/database/graphsdb.zip
+unzip graphsdb.zip
+pushd graphsdb
+for i in `ls *zip` ; do
+    unzip $i
+done
+popd
+popd
 # Build venvs
 virtualenv retworkx_venv
 retworkx_venv/bin/pip install -U retworkx
@@ -23,6 +33,7 @@ networkx_venv/bin/pip install -U networkx
 virtualenv igraph_venv
 igraph_venv/bin/pip install -U python-igraph networkx
 
+echo "Shortest path benchmarks"
 echo "Running retworkx benchmarks"
 for gr_file in dimacs_9/USA-road-1.USA.gr.gz dimacs_9/distance/USA-road-d.USA.gr.gz dimacs_9/distance/USA-road-d.NY.gr.gz dimacs_9/time/USA-road-t.USA.gr.gz dimacs_9/time/USA-road-t.NY.gr.gz dimacs_9/distance/rome99.gr; do
     retworkx_venv/bin/python retworkx_bench/shortest_path.py $gr_file
@@ -39,11 +50,17 @@ echo "Running graph-tool benchmarks"
 for gr_file in dimacs_9/USA-road-1.USA.gr.gz dimacs_9/distance/USA-road-d.USA.gr.gz dimacs_9/distance/USA-road-d.NY.gr.gz dimacs_9/time/USA-road-t.USA.gr.gz dimacs_9/time/USA-road-t.NY.gr.gz dimacs_9/distance/rome99.gr; do
     python graph_tool_bench/shortest_path.py $gr_file
 done
+echo "Isomorphism benchmarks"
+echo "Running retworkx benchmarks"
+retworkx_venv/bin/python retworkx_bench/isomorphism.py graphsdb
+echo "Running NetworkX benchmarks"
+networkx_venv/bin/python networkx_bench/isomorphism.py graphsdb
+echo "Running igraph benchmarks"
+igraph_venv/bin/python igraph_bench/isomorphism.py graphsdb
+echo "Running graph-tool benchmarks"
+python graph_tool_bench/isomorphism.py graphsdb
 
-
-# No snap shortest path benchmarks since snap doesn't offer a weighted shortest
-# path function
-
+# Process results
 mv *csv results/
 pushd results
 python ../graph_results.py
